e8872001f4efeaf78625d6ab808844e7d3b2a56d
yangyanhui <519522128@qq.com>
2019-11-07 12:08:48 +0800

    提交测试

--- app/src/main/java/songqiu/allthings/activity/MainActivity.java
+++ app/src/main/java/songqiu/allthings/activity/MainActivity.java
@@ -83,8 +83,6 @@ public class MainActivity extends BaseActivity{
     public static final int INDEX_TASK_PAGE = 2;
     public static final int INDEX_MINE_PAGE = 3;
 
-    //提交测试
-
     /**
      * 用于fragment管理
      */
