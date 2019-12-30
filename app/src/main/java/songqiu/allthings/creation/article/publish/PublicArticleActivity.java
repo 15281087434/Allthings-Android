3835ba67e232c3b7711a169a08c54179f10b7868
yangyanhui <519522128@qq.com>
2019-12-30 15:56:57 +0800

    h5联调前临时提交

--- app/src/main/java/songqiu/allthings/creation/article/publish/PublicArticleActivity.java
+++ app/src/main/java/songqiu/allthings/creation/article/publish/PublicArticleActivity.java
@@ -1,9 +1,15 @@
 package songqiu.allthings.creation.article.publish;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.Surface;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.JsPromptResult;
@@ -19,16 +25,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.heartfor.heartvideo.video.PlayerStatus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.OnClick;
-import cn.sharesdk.tencent.qq.QQ;
-import cn.sharesdk.wechat.friends.Wechat;
 import songqiu.allthings.R;
-import songqiu.allthings.activity.CommentWebViewActivity;
+import songqiu.allthings.activity.BuddingBoxingActivity;
 import songqiu.allthings.base.BaseActivity;
 import songqiu.allthings.bean.SaveArticleBean;
 import songqiu.allthings.bean.UserInfoBean;
@@ -38,17 +47,16 @@ import songqiu.allthings.http.OkHttp;
 import songqiu.allthings.http.RequestCallBack;
 import songqiu.allthings.iterface.DialogPrivacyListener;
 import songqiu.allthings.iterface.IEditTextChangeListener;
-import songqiu.allthings.mine.WithdrawActivity;
-import songqiu.allthings.mine.invite.MyFriendActivity;
-import songqiu.allthings.mine.qrcode.EwmRedEnvelopeActivity;
+import songqiu.allthings.mine.userpage.ModificationInfoActivity;
+import songqiu.allthings.util.BoxingDefaultConfig;
 import songqiu.allthings.util.ClickUtil;
 import songqiu.allthings.util.EditTextCheckUtil;
-import songqiu.allthings.util.KeyBoardUtils;
 import songqiu.allthings.util.LogUtil;
 import songqiu.allthings.util.SharedPreferencedUtils;
 import songqiu.allthings.util.StringUtil;
 import songqiu.allthings.util.ToastUtil;
 import songqiu.allthings.view.DialogArticleCommon;
+import songqiu.allthings.view.NoTitleDialog;
 
 /*******
  *
@@ -73,15 +81,13 @@ public class PublicArticleActivity extends BaseActivity {
     @BindView(R.id.shadowLayout)
     LinearLayout shadowLayout;
 
-    @BindView(R.id.titleEt)
-    EditText titleEt;
-    @BindView(R.id.contentEt)
-    EditText contentEt;
     @BindView(R.id.webview)
     WebView webView;
     WebSettings webSettings;
-    //http://192.168.0.175/share/test/example/demo/test-sperate.html
 
+
+    private ValueCallback<Uri[]> uploadMessageAboveL;
+    private final static int FILE_CHOOSER_RESULT_CODE = 10000;
     @Override
     public void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_creation_article);
@@ -94,20 +100,7 @@ public class PublicArticleActivity extends BaseActivity {
         titleTv.setText("写文章");
         rightTv.setVisibility(View.VISIBLE);
         rightTv.setText("下一步");
-        EditTextCheckUtil.textChangeListener textChangeListener = new EditTextCheckUtil.textChangeListener(rightTv);
-        textChangeListener.addAllEditText(titleEt, contentEt);
-        EditTextCheckUtil.setChangeListener(new IEditTextChangeListener() {
-            @Override
-            public void textChange(boolean isHasContent) {
-                if (isHasContent) {
-                    rightTv.setTextColor(getResources().getColor(R.color.normal_color));
-                    rightTv.setClickable(true);
-                } else {
-                    rightTv.setTextColor(getResources().getColor(R.color.FFD2D2D2));
-                    rightTv.setClickable(false);
-                }
-            }
-        });
+
         getSaveArticle();
         initWebView("http://192.168.0.175/share/test/example/demo/test-sperate.html");
     }
@@ -153,8 +146,15 @@ public class PublicArticleActivity extends BaseActivity {
         webView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 // 重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器
+                LogUtil.i("-----------shouldOverrideUrlLoading:"+url);
                 view.loadUrl(url);
                 return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                LogUtil.i("-----------onPageFinished:"+url);
+                super.onPageFinished(view, url);
             }
         });
         webView.loadUrl(url);
@@ -168,22 +168,52 @@ public class PublicArticleActivity extends BaseActivity {
 
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-//                mUploadCallbackAboveL = filePathCallback;
-//                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-//                i.addCategory(Intent.CATEGORY_OPENABLE);           i.setType("*/*");
- Intent intent = new Intent();
-
+                uploadMessageAboveL = filePathCallback;
+                Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_PICK);
                 intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-intent.setType("image/*");
-
- startActivityForResult(intent, 0x101);
-//
-//                PublicArticleActivity.this.startActivityForResult(                      Intent.createChooser(i, "File Browser"),                      0x101);           return true;
+                intent.setType("image/*");
+                startActivityForResult(intent, FILE_CHOOSER_RESULT_CODE);
                 return true;
             }
         });
+    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_CHOOSER_RESULT_CODE) { //处理返回的图片，并进行上传
+            if (null == uploadMessageAboveL) return;
+            Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
+            if (uploadMessageAboveL != null) {
+                onActivityResultAboveL(requestCode, resultCode, data);
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void onActivityResultAboveL(int requestCode, int resultCode, Intent intent) {
+        if (requestCode != FILE_CHOOSER_RESULT_CODE || uploadMessageAboveL == null)
+            return;
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (intent != null) {
+                String dataString = intent.getDataString();
+                ClipData clipData = intent.getClipData();
+                if (clipData != null) {
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+                if (dataString != null)
+                    results = new Uri[]{Uri.parse(dataString)};
+            }
+        }
+        uploadMessageAboveL.onReceiveValue(results);
+        uploadMessageAboveL = null;
     }
 
     public void getSaveArticle() {
@@ -199,8 +229,7 @@ intent.setType("image/*");
                         if (StringUtil.isEmpty(data)) return;
                         SaveArticleBean saveArticleBean = gson.fromJson(data, SaveArticleBean.class);
                         if(null == saveArticleBean) return;
-                        titleEt.setText(saveArticleBean.title);
-                        contentEt.setText(saveArticleBean.content);
+
                     }
                 });
             }
@@ -238,9 +267,38 @@ intent.setType("image/*");
 
             @Override
             public void sure() {
-                String title = titleEt.getText().toString();
-                String content = contentEt.getText().toString();
-                saveArticle(title,content);
+//                String title = titleEt.getText().toString();
+//                String content = contentEt.getText().toString();
+//                saveArticle(title,content);
+//                String ss = "?title=**&content=**";
+
+
+                webView.evaluateJavascript("javascript:getContent()", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        LogUtil.i("=========:"+value);
+                        String ss = new String(value+"");
+                        try {
+                            JSONObject jsonObject = new JSONObject(ss);
+                            LogUtil.i("---------:"+jsonObject.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+//                        String ss = value.substring(1);
+//                        String ss1 = ss.substring(0,ss.length()-1);
+//                        String sss = getUnicode(ss1);
+//                        String s1= "{\"title\":\"近距离\",\"content\":\"\u003Cp>all哦OK了\u003Cbr>\u003C/p>\"}";
+//                        getUnicode(s1);
+
+
+//                        JSONObject jsonObject = JSONObject.parseObject(value);
+//                        String title = jsonObject.getString("title");
+//                        String content = jsonObject.getString("content");
+//                        LogUtil.i("*********:"+title +" content:"+content);
+                    }
+                });
             }
         });
     }
@@ -258,6 +316,16 @@ intent.setType("image/*");
 
     }
 
+    public  String getUnicode(String s){
+        String result = "";
+        char[] c = s.toCharArray();
+        for(char tmp:c){
+          result += tmp;
+        }
+        System.out.println(result);
+        return result;
+    }
+
     @OnClick({R.id.backImg,R.id.saveTv,R.id.rightTv})
     public void onViewClick(View view) {
         switch (view.getId()) {
@@ -271,16 +339,16 @@ intent.setType("image/*");
                 break;
             case R.id.rightTv:
                 if(ClickUtil.onClick()) {
-                    String title = titleEt.getText().toString();
-                    String content = contentEt.getText().toString();
-                    if(StringUtil.isEmpty(title) || StringUtil.isEmpty(content)) {
-                        ToastUtil.showToast(this,"标题或者正文不能为空");
-                        return;
-                    }
-                    Intent intent = new Intent(this,PublicExplainActivity.class);
-                    intent.putExtra("title",title);
-                    intent.putExtra("content",content);
-                    startActivity(intent);
+//                    String title = titleEt.getText().toString();
+//                    String content = contentEt.getText().toString();
+//                    if(StringUtil.isEmpty(title) || StringUtil.isEmpty(content)) {
+//                        ToastUtil.showToast(this,"标题或者正文不能为空");
+//                        return;
+//                    }
+//                    Intent intent = new Intent(this,PublicExplainActivity.class);
+//                    intent.putExtra("title",title);
+//                    intent.putExtra("content",content);
+//                    startActivity(intent);
                 }
                 break;
         }
