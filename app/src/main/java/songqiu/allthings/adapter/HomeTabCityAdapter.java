da80753a7629144e8b670d700f274b6ba11ac89a
linyinjianying <734090232@qq.com>
2020-01-08 19:40:32 +0800

    Bug-》对齐赛字

--- app/src/main/java/songqiu/allthings/adapter/HomeTabCityAdapter.java
+++ app/src/main/java/songqiu/allthings/adapter/HomeTabCityAdapter.java
@@ -2,6 +2,7 @@ package songqiu.allthings.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
